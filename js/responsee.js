5a93531919950c9dcd7dd95c013293d2636b4663
madhavsharma1998 <36603241+madhavsharma1998@users.noreply.github.com>
2018-09-20 17:03:12 +0530

    Update responsee.js

--- js/responsee.js
+++ js/responsee.js
@@ -170,3 +170,35 @@ jQuery(document).ready(function($) {
     return this.href == url;
   }).parent('li').parent('ul').parent('li').parent('ul').parent('li').parent('ul').addClass('active-aside-item');
 });
+// Plugin options and our code
+$("#modal_trigger").leanModal({
+		top: 100,
+		overlay: 0.6,
+		closeButton: ".modal_close"
+});
+
+$(function() {
+		// Calling Login Form
+		$("#login_form").click(function() {
+				$(".social_login").hide();
+				$(".user_login").show();
+				return false;
+		});
+
+		// Calling Register Form
+		$("#register_form").click(function() {
+				$(".social_login").hide();
+				$(".user_register").show();
+				$(".header_title").text('Register');
+				return false;
+		});
+
+		// Going back to Social Forms
+		$(".back_btn").click(function() {
+				$(".user_login").hide();
+				$(".user_register").hide();
+				$(".social_login").show();
+				$(".header_title").text('Login');
+				return false;
+		});
+});
